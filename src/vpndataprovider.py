acb174aef5cba7efc81e3e0494e15168eaf2ff36
thiago-dev-cyber <thiagof@tutamail.com>
2026-01-25 22:21:23 -0300

    config: melhorando e adicionando documentação aos metodos.

--- src/vpndataprovider.py
+++ src/vpndataprovider.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import json
 from abc import ABC, abstractmethod
 
 
@@ -8,11 +9,11 @@ class VpnProvider(ABC):
 	Classe Abstrata que serve como base para a criação das outras.
 	"""
 	# https://pt.wikipedia.org/wiki/Alian%C3%A7a_Cinco_Olhos
-	five_eyes = ["EUA", "UK", "CA", "AU", "NZ"]                        #Type 1
-	nine_eyes = five_eyes + ["DK", "FR", "NL" ,"NO"]                   #Type 2
-	fourheen_eyes  = nine_eyes + ["DE", "BE", "IT", "ES", "SE"]       #Type 3
+	five_eyes = ["EUA", "UK", "CA", "AU", "NZ"]                        #eyes_type 5
+	nine_eyes = five_eyes + ["DK", "FR", "NL" ,"NO"]                   #eyes_type 9
+	fourheen_eyes  = nine_eyes + ["DE", "BE", "IT", "ES", "SE"]        #eyes_type 14
 
-	@staticmethod
+	@abstractmethod
 	def cache_is_valid(ttl=3600):
 		pass
 
@@ -28,7 +29,7 @@ class VpnProvider(ABC):
 	def feth_raw(self) -> list:
 		pass
 
-
+	# Modifica dos dados coletados com o objetivo de facilitar a manipulação.
 	@abstractmethod
 	def transformdata(self, raw_data: dict) -> list:
 		pass
@@ -39,18 +40,48 @@ class MullvadProvider(VpnProvider):
 	"""
 	Obtem e Filtra dados sobre os servidores que o provedor Mullvad disponibiliza para se conectar.
 
-	Methodos:
+	Metodos:
 		fetch_raw: Responsavel por se conectar a API e coletar os dados.
+
+		cache_is_valid: Verifica se o cache dos servidores excedeu o tempo de vida maximo TTL.
+
+		clean_data: Remove os paises dos 4, 9 e 14 olhos da lista de servidores.
+
+		transformdata: Modifica a lista de servidores a fim de deixa-la util.
+
 	"""
 	def __init__(self):
 		self.api_url = "https://api.mullvad.net/www/relays/wireguard/"
+		self._servers = {}
 
 
-	def feth_raw(self):
+	# Tempo de vida do cache 1h (3600 segundos)
+	def cache_is_valid(ttl: int = 3600):
+		"""
+		Verifica se o cache salvo ainda está valido.
+
+		Argumentos:
+
+			ttl (int): Tempo de vida em segundos a ser verificado.
+			Valor padrão 1 hora (3600)
+		"""
+		current_time = time.time()
+		if current_time - self._servers['ttl_cache'] > ttl:
+			return False
+
+
+	def feth_raw(self) -> list:
+		"""
+		Coleta a lista de servidores disponiveis a partir da API disponibilizada pela Mullvad.
+
+		Retorno:
+
+			Lista de servidores disponiveis fornecida pela Mullvad.
+		"""
 		try:
 			response = requests.get(self.api_url)
 			if response.status_code == 200:
-				raw_data = response.json()
+				raw_data = json.loads(response.text)
 				print("Informações dos servidores obtida com Sucesso.")
 				return raw_data
 
@@ -59,11 +90,27 @@ class MullvadProvider(VpnProvider):
 
 
 	@classmethod
-	def clean_data(cls, raw_data: dict, eyes_type: int):
+	def clean_data(cls, raw_data: list, eyes_type: int) -> list:
+		"""
+		Retira da lista servidores que fazem parte dos 5, 9 e 14 olhos
+
+		Argumentos:
+			
+			raw_data: Lista contendo os servidores
+
+			eyes_type: Tipo de tratado usado como base. Valor esperado: 5, 9 ou 14
+
+				five_eyes = ["EUA", "UK", "CA", "AU", "NZ"]                        eyes_type 5
+				nine_eyes = five_eyes + ["DK", "FR", "NL" ,"NO"]                   eyes_type 9
+				fourheen_eyes  = nine_eyes + ["DE", "BE", "IT", "ES", "SE"]        eyes_type 14
+
+
+		Retorno: Uma lista sem os paises selecionados.
+		"""
 		eyes_map = {
-			1 : cls.five_eyes,
-			2 : cls.nine_eyes,
-			3 : cls.fourheen_eyes
+			5  : cls.five_eyes,
+			9  : cls.nine_eyes,
+			14 : cls.fourheen_eyes
 		}
 
 		countries = eyes_map.get(eyes_type)
@@ -78,5 +125,26 @@ class MullvadProvider(VpnProvider):
 		return	clean_servers
 
 
-	def transformdata(self, raw_data: dict) -> list:
-		pass
+	def transformdata(self, raw_data: list) -> list:
+		"""
+		Agrupa os servidores pelo codigo do pais (BR, EUA...), facilitando o controle.
+
+		Argumentos:
+
+			raw_data: Lista com os servidores a serem agrupados.
+
+		Retorno:
+
+			lista com os servidores agrupados.
+		"""
+		servers  = {}
+		for server in raw_data:
+			country = server['country_code'].upper()
+
+			if country in servers.keys():
+				servers[country].append(server)
+
+			else:
+				servers[country] = [server]
+
+		return servers
