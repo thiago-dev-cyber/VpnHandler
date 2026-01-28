85b30ffa5a9f8b75ce92158ee74e34eca74355e7
thiago-dev-cyber <thiagof@tutamail.com>
2026-01-28 17:44:22 -0300

    config: idealizando o modulo config.
    Modulo responsavel por controlar as configurações do projeto.

---
+++ src/config/config.py
@@ -0,0 +1,9 @@
+import json
+import os
+
+
+CONFIG_DIR = os.path.dirname(os.path.abspath(__file__))
+SOURCE_DIR = os.path.dirname(CONFIG_DIR)
+ROOT_DIR = os.path.dirname(SOURCE_DIR)
+
+# TODO: Importar as Variaveis de configuração da VPN
